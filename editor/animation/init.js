9aabcd833dd464aceec304dfc0d24664d0ddfb48
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-11-27 10:51:39 +1200

    tryit

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -85,6 +85,51 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 
         });
 
+        var $tryit;
+        var tCanvas;
+
+        ext.set_console_process_ret(function (this_e, ret) {
+            try {
+                ret = JSON.parse(ret);
+            }
+            catch (err) {
+            }
+            $tryit.find(".checkio-result").html("Result:<br>" + JSON.stringify(ret));
+        });
+
+        ext.set_generate_animation_panel(function (this_e) {
+
+            $tryit = $(this_e.setHtmlTryIt(ext.get_template('tryit'))).find('.tryit-content');
+            tCanvas = new CakesCanvas({cell: 20, x0: 10, y0: 10});
+            tCanvas.prepareCanvas($tryit.find(".tryit-canvas")[0]);
+            tCanvas.createCanvasTryit([
+                [2, 2],
+                [8, 8],
+                [2, 8],
+                [8, 2],
+                [5, 5]
+            ]);
+            tCanvas.createFeedback();
+
+            $tryit.find(".bn-clear").click(function (e) {
+                tCanvas.clearCanvas();
+                e.stopPropagation();
+                return false;
+            });
+            $tryit.find(".bn-random").click(function (e) {
+                tCanvas.randomCanvas();
+                e.stopPropagation();
+                return false;
+            });
+
+            $tryit.find(".bn-check").click(function (e) {
+
+                this_e.sendToConsoleCheckiO(tCanvas.gatherData());
+                e.stopPropagation();
+                return false;
+            });
+        });
+
         function CakesCanvas(options) {
             var format = Raphael.format;
 
@@ -120,11 +165,13 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             var paper;
             var cakeSet;
 
-            var attrBackLine = {"stroke": colorBlue3, "stroke-width": 1, "stroke-dasharray": "- "};
+            var attrBackLine = {"stroke": colorBlue3, "stroke-width": 0.5, "stroke-dasharray": "- "};
             var attrLine = {"stroke": colorOrange4, "stroke-width": 3};
-            var attrCake = {"stroke": colorBlue4, "fill": colorBlue4};
+            var attrCake = {"stroke": colorBlue3, "fill": colorBlue3};
+            var obj = this;
+            var active;
 
-            this.prepareCanvas = function(dom) {
+            this.prepareCanvas = function (dom) {
                 paper = Raphael(dom, sizeX, sizeY);
                 for (var i = 1; i < 10; i++) {
                     paper.path(format("M{0},{1}H{2}",
@@ -136,7 +183,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 
             };
 
-            this.createCanvas = function(cakes, lines) {
+            this.createCanvas = function (cakes, lines) {
                 cakeSet = paper.set();
                 for (var i = 0; i < cakes.length; i++) {
                     cakeSet.push(paper.circle(x0, y0, rCake).attr(attrCake));
@@ -145,7 +192,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                     );
 
                 }
-                setTimeout(function(){
+                setTimeout(function () {
                     for (var j = 0; j < lines.length; j++) {
                         var cl = lines[j];
                         var line = paper.path(format(
@@ -161,7 +208,82 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                             y0 + cl[3] * cell)}, 200);
                     }
                 }, 200);
+            };
+
+
+            this.createCanvasTryit = function (defLayout) {
+                cakeSet = paper.set();
+
+                for (var i = 0; i < defLayout.length; i++) {
+                    var c = paper.circle(x0 + defLayout[i][0] * cell, y0 + defLayout[i][1] * cell, rCake).attr(attrCake).attr({"fill-opacity": 0, "opacity": 0});
+                    c.animate({"fill-opacity": 1, "opacity": 1}, 100);
+                    c.x = defLayout[i][0];
+                    c.y = defLayout[i][1];
+                    cakeSet.push(c);
+                }
+            };
+
+
+            this.createFeedback = function () {
+
+                var removeCircle = function () {
+                    cakeSet.exclude(this);
+                    this.animate({"fill-opacity": 0, "opacity": 0}, 100, function () {
+                        this.remove();
+                    });
+                };
+                cakeSet.click(removeCircle);
+                if (!active) {
+                    active = paper.rect(x0, y0, cell * 10, cell * 10).attr({"fill": colorWhite, "fill-opacity": 0, "stroke-width": 0});
+
+                    active.click(function (e) {
+                        var x = Math.round((e.offsetX - x0) / cell);
+                        var y = Math.round((e.offsetY - y0) / cell);
+                        var c = paper.circle(x0 + x * cell, y0 + y * cell, rCake).attr(attrCake)
+                        c.animate({"fill-opacity": 1, "opacity": 1}, 100);
+                        c.click(removeCircle);
+                        c.x = x;
+                        c.y = y;
+                        cakeSet.push(c);
+                    });
+                }
+                cakeSet.toFront();
+
+            };
+
+            this.gatherData = function () {
+                var res = [];
+                for (var i = 0; i < cakeSet.length; i++) {
+                    res.push([cakeSet[i].x, cakeSet[i].y]);
+                }
+                return res;
+            };
+
+            this.clearCanvas = function () {
+                cakeSet.forEach(function (c) {
+                    c.animate({"fill-opacity": 0, "opacity": 0}, 100, function () {
+                        this.remove();
+                    });
+                });
+                cakeSet.clear();
+            };
+
+            this.randomCanvas = function () {
+                var quant = Math.floor(Math.random() * 15 + 5);
+                var cakes = [];
+                for (var i = 0; i < quant; i++) {
+                    var x = Math.floor(Math.random() * 9 + 1);
+                    var y = Math.floor(Math.random() * 9 + 1);
+                    if (cakes.join(";").indexOf(x + "," + y) === -1) {
+                        cakes.push([x, y]);
+                    }
+                }
+                obj.clearCanvas();
+                obj.createCanvasTryit(cakes);
+                obj.createFeedback();
+
             }
+
 
         }
 
